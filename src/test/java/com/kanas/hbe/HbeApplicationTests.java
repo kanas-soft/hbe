5680ebc7e616d94a391b346b2bdb6fb3abfedd0b
TodorKV <todorv1@gmail.com>
2023-05-25 00:54:15 +0300

    added spring boot 3.1.0 java 17 mvn project

---
+++ src/test/java/com/kanas/hbe/HbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanas.hbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
