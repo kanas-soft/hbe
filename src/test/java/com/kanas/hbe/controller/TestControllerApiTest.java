5bb26764b6e505643ed36c15fd8918686bb6f472
TodorKV <todorv1@gmail.com>
2023-05-26 18:06:12 +0300

    added apitest for TestController

---
+++ src/test/java/com/kanas/hbe/controller/TestControllerApiTest.java
@@ -0,0 +1,23 @@
+package com.kanas.hbe.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+public class TestControllerApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World!"));
+    }
+
+}
