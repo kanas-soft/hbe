d739f21d9505f6df5c5c7913a3103835ff92fd58
TodorKV <todorv1@gmail.com>
2023-05-27 13:52:16 +0300

    trying new naming convetions to see if coverage 0.0 will get changed.

--- src/main/java/com/kanas/hbe/controller/TestController.java
+++ src/main/java/com/kanas/hbe/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
+    public String helloWorld() {
         return "Hello World!";
     }
 
