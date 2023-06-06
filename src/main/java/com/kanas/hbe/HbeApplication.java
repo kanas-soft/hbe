e11e343d40427dd747425d094795651ce3e75d3f
ZdravkoM33 <zdravko.mld@gmail.com>
2023-06-06 20:28:45 +0300

    feature: first set-up of registration process

--- src/main/java/com/kanas/hbe/HbeApplication.java
+++ src/main/java/com/kanas/hbe/HbeApplication.java
@@ -2,8 +2,10 @@ package com.kanas.hbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.kanas.hbe"})
 public class HbeApplication {
 
 	public static void main(String[] args) {
