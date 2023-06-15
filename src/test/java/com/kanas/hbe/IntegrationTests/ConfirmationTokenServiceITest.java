6ab2486e87dd1c7783d6d06569d369b790c8bc80
TodorKV <todorv1@gmail.com>
2023-06-15 14:35:39 +0300

    - More tests

--- src/test/java/com/kanas/hbe/IntegrationTests/ConfirmationTokenServiceITest.java
+++ src/test/java/com/kanas/hbe/IntegrationTests/ConfirmationTokenServiceITest.java
@@ -1,6 +1,9 @@
 package com.kanas.hbe.IntegrationTests;
 
 import com.kanas.hbe.domain.entity.ConfirmationToken;
+import com.kanas.hbe.domain.entity.User;
+import com.kanas.hbe.exception.InvalidConfirmationTokenException;
+import com.kanas.hbe.fixtures.UserFixtures;
 import com.kanas.hbe.repository.ConfirmationTokenRepository;
 import com.kanas.hbe.service.impl.ConfirmationTokenServiceImpl;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +12,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
 import java.util.Optional;
+import java.util.UUID;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,7 +31,7 @@ public class ConfirmationTokenServiceITest {
 
     @Nested
     @DisplayName("Get Confirmation Token ITests")
-    class getConfirmationToken {
+    class GetConfirmationToken {
 
         @Test
         void whenFetching_byToken_NoErrors() throws Exception {
@@ -47,6 +53,66 @@ public class ConfirmationTokenServiceITest {
 
             // Then
             assertFalse(optionalConfirmationToken.isPresent());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Save Confirmation Token ITests")
+    class SaveConfirmationToken {
+
+        @Test
+        void whenCreating_Token_NoErrors() throws Exception {
+
+            // Given
+            User user = UserFixtures.createUser();
+
+            // This is existing user in the h2 db.
+            user.setId(UUID.fromString("de305d54-75b4-431b-adb2-eb6b9e546014"));
+
+            // When
+            confirmationTokenService.createConfirmationToken(user, "8ce266f3-18c7-4c61-892d-04f54a929c8f");
+
+            // Then
+            Optional<ConfirmationToken> optionalConfirmationToken = confirmationTokenService
+                    .getConfirmationToken("8ce266f3-18c7-4c61-892d-04f54a929c8f");
+
+            assertTrue(optionalConfirmationToken.isPresent());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Regenerate Confirmation Token ITests")
+    class RegenerateConfirmationToken {
+
+        @Test
+        void whenRegenerating_Token_Error() throws Exception {
+
+            // Given
+            // Token thats not in the h2 db.
+            String uuid = "8ce266ff-18c7-4c61-892d-04f54a929c8f";
+
+            // When Then
+            assertThrows(InvalidConfirmationTokenException.class,
+                    () -> confirmationTokenService.regenerateConfirmationToken(uuid));
+        }
+
+        @Test
+        void whenRegenerating_Token_NoError() throws Exception {
+
+            // Given
+            // Token that exists in the h2 db.
+            String token = "8ce266f2-18c7-4c61-892d-04f54a929c8f";
+            LocalDateTime dateTime1 = LocalDateTime.of(2023, 6, 14, 16, 0, 0);
+
+            // When
+            ConfirmationToken confirmationToken = confirmationTokenService.regenerateConfirmationToken(token);
+
+            // Then
+            assertNotNull(confirmationToken);
+            assertNotEquals(token, confirmationToken.getToken());
+            assertTrue(dateTime1.isBefore(confirmationToken.getExpiryDate()));
         }
 
     }
