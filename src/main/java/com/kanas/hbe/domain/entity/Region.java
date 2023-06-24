418a6dd04c219aa2598ee37a9d71751737f30e4f
ZdravkoM33 <zdravko.mld@gmail.com>
2023-06-24 15:55:47 +0300

    feature (regions): creation of region entity with many-to-many relation with handyman table

---
+++ src/main/java/com/kanas/hbe/domain/entity/Region.java
@@ -0,0 +1,31 @@
+package com.kanas.hbe.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "regions")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region extends BaseEntity {
+
+    @Column
+    private String city;
+
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(
+            name = "handymen_regions",
+            joinColumns = @JoinColumn(name = "handyman_id"),
+            inverseJoinColumns = @JoinColumn(name = "region_id")
+    )
+    private Set<Handyman> handymen = new HashSet<>();
+}
